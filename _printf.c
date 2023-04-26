a651ffc5383876713990df58c0a3260d8d1b876b
Bimacobiz <collinsmahigi@gmail.com>
2023-04-26 15:29:01 -0700

    ammendment

--- _printf.c
+++ _printf.c
@@ -31,15 +31,15 @@ int _printf(const char *format, ...)
 			printed_chars++;
 		}
 		else
-		{	
-                        print_buffer(buffer, &buff_ind);
-                        flags = get_flags(format, &i);
-                        width = get_width(format, &i, list);
-                        precision = get_precision(format, &i, list);
-                        size = get_size(format, &i);
-                        ++i;
-                        printed = handle_print(format, &i, list, buffer,
-                                        flags, width, precision, size);
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -47,15 +47,15 @@ int _printf(const char *format, ...)
 	}
 	return (printed_chars);
 }
-	/**
-	 * print_buffer - Prints the contents of the buffer if it exist
-	 * @buffer: Array of chars
-	 * @buff_ind: Index at which to add next char, represents the length.
-	 */
-	void print_buffer(char buffer[], int *buff_ind)
-	{
-		if (*buff_ind > 0)
-			write(1, &buffer[0], *buff_ind);
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-		*buff_ind = 0;
-	}
+	*buff_ind = 0;
+}
